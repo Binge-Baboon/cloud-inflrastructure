c2c3ee7c0285f9e13a713bf201c4c72ddc88e895
Ilija <ilija.beslin@gmail.com>
2024-05-28 12:04:32 +0200

    feat: added movie service

---
+++ movies-service/createMovie/create_movie.py
@@ -0,0 +1,30 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+from shared.utils import create_response
+
+def create(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Movies')
+
+    data = json.loads(event['body'])
+    movie_id = data['id']
+    title = data['title']
+    genres = data['genres']
+    actors = data['actors']
+    directors = data['directors']
+
+    try:
+        table.put_item(
+            Item={
+                'id': movie_id,
+                'title': title,
+                'genres': genres,
+                'actors': actors,
+                'directors': directors
+            }
+        )
+        return create_response(201, {'message': 'Movie created successfully!'})
+    except ClientError as e:
+        return create_response(e.response['ResponseMetadata']['HTTPStatusCode'], e.response['Error']['Message'])
---
+++ movies-service/getMovie/get_movie.py
@@ -0,0 +1,20 @@
+import json
+import boto3
+
+from shared.utils import create_response
+
+def get_one(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Movies')
+
+    movie_id = event['pathParameters']['id']
+    # movie_id = event['rawPath'][1:]
+    response = table.get_item(
+        Key={
+            'id': int(movie_id)
+        }
+    )
+    if 'Item' in response:
+        return create_response(200, response['Item'])
+    else:
+        return create_response(404, {'message': 'Movie not found'})
