e02dca87b465915992a73b61c54bd3f436d937c9
markomitos <markomitosevic9@gmail.com>
2024-05-28 17:13:43 +0200

    feat: added tvshow service

---
+++ tvshows-service/createTVShow/create_tvshow.py
@@ -0,0 +1,34 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+from shared.utils import create_response
+
+def create(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('TVShows')
+
+    data = json.loads(event['body'])
+    tvshow_id = data['id']
+    title = data['title']
+    description = data['description']
+    genres = data['genres']
+    actors = data['actors']
+    directors = data['directors']
+    rating = data['rating']
+
+    try:
+        table.put_item(
+            Item={
+                'id': tvshow_id,
+                'title': title,
+                'description': description,
+                'genres': genres,
+                'actors': actors,
+                'directors': directors,
+                'rating':rating
+            }
+        )
+        return create_response(201, {'message': 'TVShow created successfully!'})
+    except ClientError as e:
+        return create_response(e.response['ResponseMetadata']['HTTPStatusCode'], e.response['Error']['Message'])
---
+++ tvshows-service/updateTVShow/update_tvshow.py
@@ -0,0 +1,54 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+from shared.utils import create_response
+
+def update(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('TVShows')
+
+    data = json.loads(event['body'])
+    tvshow_id = event['pathParameters']['id']
+
+    update_expression = "SET "
+    expression_attribute_values = {}
+
+    if 'title' in data:
+        update_expression += "title = :title, "
+        expression_attribute_values[':title'] = data['title']
+
+    if 'description' in data:
+        update_expression += "description = :description, "
+        expression_attribute_values[':description'] = data['description']
+
+    if 'genres' in data:
+        update_expression += "genres = :genres, "
+        expression_attribute_values[':genres'] = data['genres']
+
+    if 'actors' in data:
+        update_expression += "actors = :actors, "
+        expression_attribute_values[':actors'] = data['actors']
+
+    if 'directors' in data:
+        update_expression += "directors = :directors, "
+        expression_attribute_values[':directors'] = data['directors']
+
+    if 'rating' in data:
+        update_expression += "rating = :rating, "
+        expression_attribute_values[':rating'] = data['rating']
+
+    update_expression = update_expression.rstrip(', ')
+
+    try:
+        table.update_item(
+            Key={
+                'id': int(tvshow_id)
+            },
+            UpdateExpression=update_expression,
+            ExpressionAttributeValues=expression_attribute_values
+        )
+        return create_response(200, {'message': 'TVShow updated successfully!'})
+
+    except ClientError as e:
+        create_response(e.response['ResponseMetadata']['HTTPStatusCode'], e.response['Error']['Message'])
