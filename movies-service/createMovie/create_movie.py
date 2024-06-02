0e52fcbae0fb20b164368c87f9e60245693c2bf9
Ilija <ilija.beslin@gmail.com>
2024-06-02 20:02:07 +0200

    feat: image handling

--- movies-service/createMovie/create_movie.py
+++ movies-service/createMovie/create_movie.py
@@ -1,12 +1,17 @@
+import base64
 import json
 import boto3
 from botocore.exceptions import ClientError
 
 from shared.utils import create_response
 
+
 def create(event, context):
     dynamodb = boto3.resource('dynamodb')
+    s3 = boto3.client('s3')
+
     table = dynamodb.Table('Movies')
+    bucket_name = 'binge-baboon-images'  # replace with your S3 bucket name
 
     data = json.loads(event['body'])
     movie_id = data['id']
@@ -14,9 +19,18 @@ def create(event, context):
     genres = data['genres']
     actors = data['actors']
     directors = data['directors']
-    image_key = data['image_key']
+    image_data = data['image_data']  # base64 encoded image data
+
+    image_key = f"{movie_id}.jpg"  # or any other naming convention
 
     try:
+        # Decode the base64 image data
+        image_binary = base64.b64decode(image_data)
+
+        # Upload the image to S3
+        s3.put_object(Bucket=bucket_name, Key=image_key, Body=image_binary, ContentType='image/jpeg')
+
+        # Store the movie data with image key in DynamoDB
         table.put_item(
             Item={
                 'id': movie_id,
--- movies-service/getMovie/get_movie.py
+++ movies-service/getMovie/get_movie.py
@@ -1,20 +1,36 @@
 import json
 import boto3
+import base64
 
 from shared.utils import create_response
 
+
 def get_one(event, context):
     dynamodb = boto3.resource('dynamodb')
+    s3 = boto3.client('s3')
+
     table = dynamodb.Table('Movies')
+    bucket_name = 'binge-baboon-images'  # replace with your S3 bucket name
 
     movie_id = event['pathParameters']['id']
-    # movie_id = event['rawPath'][1:]
     response = table.get_item(
         Key={
             'id': int(movie_id)
         }
     )
+
     if 'Item' in response:
-        return create_response(200, response['Item'])
+        item = response['Item']
+        if 'image_key' in item:
+            image_key = item['image_key']
+            try:
+                s3_response = s3.get_object(Bucket=bucket_name, Key=image_key)
+                image_data = s3_response['Body'].read()
+                image_base64 = base64.b64encode(image_data).decode('utf-8')
+                item['image_data'] = image_base64
+            except Exception as e:
+                print(f"Error fetching image from S3: {e}")
+                item['image_data'] = None
+        return create_response(200, item)
     else:
         return create_response(404, {'message': 'Movie not found'})
