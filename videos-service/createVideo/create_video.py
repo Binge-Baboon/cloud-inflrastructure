3ca154b1a76e617a2c8a73f9f99bf6907eb719f5
markomitos <markomitosevic9@gmail.com>
2024-06-06 15:31:27 +0200

    feat: added video upload

--- videos-service/createVideo/create_video.py
+++ videos-service/createVideo/create_video.py
@@ -1,4 +1,5 @@
 import json
+import base64
 import boto3
 from botocore.exceptions import ClientError
 
@@ -6,20 +7,30 @@ from shared.utils import create_response
 
 def create(event, context):
     dynamodb = boto3.resource('dynamodb')
+    s3 = boto3.client('s3')
+
     table = dynamodb.Table('Videos')
+    bucket_name = 'binge-baboon-videos'
 
     data = json.loads(event['body'])
-    movie_id = data['id']
+    video_id = data['id']
     size = data['size']
     datatype = data['datatype']
     modified = data['modified']
     created = data['created']
-    s3key = data['s3key']
+    video_data = data['video_data']
+    s3key = f"{video_id}.mp4"
+
 
     try:
+        video_binary = base64.b64decode(video_data)
+
+        s3.put_object(Bucket=bucket_name, Key=s3key, Body=video_binary, ContentType='video/mp4')
+
+
         table.put_item(
             Item={
-                'id': movie_id,
+                'id': video_id,
                 'size': size,
                 'datatype': datatype,
                 'modified': modified,
